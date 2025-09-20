b1d1cd2fd69dc0882c9599f0383ffbcf36d51040
tmb-spec <tassenmesserbande@gmail.com>
2025-09-20 21:03:08 +0200

    Update Galerie Hover-Effekt auf scale-150

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,9 @@ const [images] = useState([
   "/images/bild4.jpg",
   "/images/bild5.jpg",
   "/images/bild6.jpg",
+  "/images/bild7.jpg",
+  "/images/bild8.jpg",
+  "/images/bild9.jpg"
 ]);
 
   const [showSuccess, setShowSuccess] = useState(false);
@@ -65,18 +68,20 @@ const [images] = useState([
 
       {/* Gallery */}
       <section id="gallery" className="max-w-6xl mx-auto py-16 px-4">
-        <h3 className="text-3xl font-bold mb-6 text-green-700 text-center md:text-left">Galerie</h3>
+        <h3 className="text-3xl font-bold mb-6 text-green-700 text-center md:text-left">
+    Galerie
+        </h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {images.map((img, i) => (
-            <img
-              key={i}
-              src={img}
-              alt={`Bild ${i + 1}`}
-              className="rounded-xl shadow-md w-full object-cover"
-            />
-          ))}
-        </div>
-      </section>
+    {images.map((img, i) => (
+      <img
+        key={i}
+        src={img}
+        alt={`Bild ${i + 1}`}
+        className="rounded-xl w-full h-72 object-cover transform transition-transform duration-500 hover:scale-150"
+      />
+    ))}
+  </div>
+</section>
 
       {/* Team / Steckbriefe */}
       <section id="team" className="max-w-6xl mx-auto py-16 px-4">
